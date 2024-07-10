3e2befa59111d04a0aa62fb568c45b8ee9f4930f
ishanrana363 <ishanrana363@gmail.com>
2024-07-11 00:55:30 +0600

    Tailwind font family

---
+++ Tailwind_Font_Family/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily:{
+      'display' : ["Oswald"],
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
