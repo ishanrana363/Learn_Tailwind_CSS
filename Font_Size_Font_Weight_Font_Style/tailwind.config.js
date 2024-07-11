54a63d12cf5ff0fccd5847b33a03e06c03e1a2f2
ishanrana363 <ishanrana363@gmail.com>
2024-07-11 21:20:20 +0600

    font style font size font weight

---
+++ Font_Size_Font_Weight_Font_Style/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontWeight : {
+
+      bolders : ["700"]
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
